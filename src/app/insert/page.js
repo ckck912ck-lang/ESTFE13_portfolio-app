232813b144afcfb7fb4ca15bdc020f50d8501d17
ckck912ck-lang <ckck912ck@gmail.com>
2026-08-06 10:39:00 +0900

    Add user authentication and logout functionality; implement LoginStatus component

--- src/app/insert/page.js
+++ src/app/insert/page.js
@@ -1,7 +1,7 @@
 "use client";
-
+6;
 import { createClient } from "@/utils/supabase/client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Insert() {
@@ -22,6 +22,20 @@ export default function Insert() {
   });
 
   const [thumbnail, setThumbnail] = useState(null);
+  const [user, setUser] = useState(null);
+  const [authForm, setAuthform] = useState({
+    email: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    (async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+    })();
+  }, [supabase.auth]);
 
   async function insertData(e) {
     e.preventDefault();
@@ -32,7 +46,11 @@ export default function Insert() {
       console.log("데이터 입력 성공");
       router.push("/");
     }
+    if (thumbnail) {
+      await uploadThumbnail(thumbnail);
+    }
   }
+
   const handleChange = e => {
     const { name, value } = e.target;
 
@@ -42,9 +60,76 @@ export default function Insert() {
     });
   };
 
+  const handleAuthChange = e => {
+    const { name, value } = e.target;
+    setAuthform(prev => ({ ...prev, [name]: value }));
+  };
+
   const handleFileChange = e => {
     setThumbnail(e.target.files[0]);
   };
+
+  async function uploadThumbnail(file) {
+    const { data, error } = await supabase.storage
+      .from("portfolio")
+      .upload(`thumbnail/${file.name}`, file);
+    if (error) {
+      // Handle error
+      console.error("파일 업로드 실패:", error);
+    } else {
+      // Handle success
+      console.log("파일 업로드 성공:");
+    }
+  }
+
+  //로그인 진행
+  const handleLogin = async e => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signInWithPassword(authForm);
+    if (error) {
+      alert("로그인 실패", error.message);
+    } else {
+      alert("로그인 성공");
+      router.refresh();
+    }
+  };
+
+  if (!user) {
+    return (
+      <div className="about_content shadow">
+        <h2>관리자 로그인</h2>
+        <div className="contact_form">
+          <form onSubmit={handleLogin}>
+            <p className="field">
+              <label htmlFor="email">이메일</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="email"
+                required
+                onChange={handleAuthChange}
+              />
+            </p>
+            <p className="field">
+              <label htmlFor="password">비밀번호</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="비밀번호"
+                required
+                onChange={handleAuthChange}
+              />
+            </p>
+            <p className="submit">
+              <input type="submit" className="primary-btn" value="로그인" />
+            </p>
+          </form>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="about_content shadow">
